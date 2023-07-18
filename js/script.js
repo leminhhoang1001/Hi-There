de3e9a74acf2ea467266b0b705c5b55c26460c3a
leminhhoang1001 <32322377+leminhhoang1001@users.noreply.github.com>
2023-07-18 18:14:50 +0700

    commit

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
     const b = $('body');
     const top = $('<div class="top"></div>'),
         bot = $('<div class="bot"></div>'),
-        rickroll = $('<div  class="rickroll"><img class="img-rickroll" src="./img/rickrolled.gif" alt=""><img src="https://barefoot.pics/image.php?id=N2TTPN.png" class="aaa"></div>');
+        rickroll = $('<div onload="rickrollOn()" class="rickroll"><img class="img-rickroll" src="./img/rickrolled.gif" alt=""><img src="https://barefoot.pics/image.php?id=N2TTPN.png" class="aaa"></div>');
     b.append(top.add(bot));
     function rickrollOn() {
         b.append(rickroll);
@@ -15,7 +15,9 @@
           autoplay: true,
           html5: true
         });
-        audio.play();
+        audio.once('load', function(){
+          audio.play();
+        });
     };
     b.one('click', function(e){
         e.preventDefault();
