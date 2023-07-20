2141cbea5360408caf6674b122cda2dba308e6c8
leminhhoang1001 <32322377+leminhhoang1001@users.noreply.github.com>
2023-07-20 21:21:55 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
     //lets do it
     $('html, body').css({overflow: 'hidden'});
     const b = $('body');
-    // const audio = $("#RR");
+    const audio = $("#RR");
     const top = $('<div class="top"></div>'),
         bot = $('<div class="bot"></div>'),
         rickroll = $('<div class="rickroll"><audio id="RR" playsinline autoplay><!-- ACC file (chrome/Safari/IE) --><source src="./rickrolled.m4a" type="audio/mpeg" /><!-- Ogg Vorbis (Firefox) --><source src="./rickrolled.ogg" type="audio/ogg" /><!-- Mp3 --><source src="./rickrolled.mp3" type="audio/mpeg"></audio><img class="img-rickroll" src="./img/rickrolled.gif" alt=""></div>');
@@ -23,12 +23,12 @@
                 top.css({height: 100 + '%'})
                 bot.animate({width: 0, left: 50 +'%'}, 400)
             });
-            // audio[0].play();
         }, 2000);
         setTimeout(rickrollOn ,3000);
     });
     function rickrollOn() {
       b.append(rickroll);
+      audio[0].play();
 
 
       // document.body.addEventListener('touchstart', function() {
